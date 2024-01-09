dd8020f04c0941429589e3d08e6c698af561df8c
Fernando Carta UBUNTU <cartafernando@outlook.com>
2024-01-09 11:54:34 -0500

    Ajustes en la instalación PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
   title = 'dieta-app';
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+import { ButtonModule } from 'primeng/button';
+
+
+@NgModule({
+    declarations:[
+        
+    ],
+    imports: [
+        BrowserModule,
+        ButtonModule
+    ],
+    providers: [],
+    
+})
+
+export class AppModule{}
